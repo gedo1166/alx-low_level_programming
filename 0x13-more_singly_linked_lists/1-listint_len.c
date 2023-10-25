8700c0e1c9e537857e9ccef35ee4e29d4f67453b
Mohamed Abdallah <gedomega5@gmail.com>
2023-10-26 02:05:57 +0300

    fix code style

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,17 +2,16 @@
 
 /**
  * listint_len - returns the number of elements in a linked lists
- * @h: linked the list of type listint_t to traverse
- * Return: number of nodes
+ * @h: the linked list of type listint_t to traverse
+ * Return: the number of nodes
  */
- 
 size_t listint_len(const listint_t *h)
 {
-	size_t num = 0;
+	size_t x = 0;
 
 	while (h)
 	{
-		num++;
+		x++;
 		h = h->next;
 	}
 
