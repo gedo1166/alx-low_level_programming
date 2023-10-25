38034a645f5d7d981e2bcb9259009d587a8578a1
Mohamed Abdallah <gedomega5@gmail.com>
2023-10-26 01:39:45 +0300

    add more singly linked lists task

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * @h: linked the list of type listint_t to traverse
+ * listint_len - returns the number of elements in a linked lists
+ * Return: number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		num++;
+		h = h->next;
+	}
+
+	return (num);
+}
